a99c9da7150eb72d13bc81c881d11292a1c03e55
Amber Jain <jainanuamber@gmail.com>
2024-08-23 12:38:33 +0530

    maven publish setup ✅

--- kotlin_client/build.gradle.kts
+++ kotlin_client/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    `maven-publish`
 }
 
 android {
@@ -33,11 +34,23 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.huddle01.kotlin_client"
+            artifactId = "kotlin-client"
+            version = "1.0.0"
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
 }
