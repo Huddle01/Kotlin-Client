65b9983246c8a5e3b652b05880c292eba864cae5
Amber Jain <jainanuamber@gmail.com>
2024-08-23 16:12:12 +0530

    commented local module use

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(project(":kotlin_client"))
+//    implementation(project(":kotlin_client"))
 }
