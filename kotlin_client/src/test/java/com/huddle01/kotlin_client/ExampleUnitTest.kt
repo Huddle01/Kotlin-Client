ca6468e094999043682f297ebaf5d2f377ab57ee
Amber Jain <jainanuamber@gmail.com>
2024-11-04 17:18:10 +0530

    reformated kotlin_client package

--- kotlin_client/src/test/java/com/huddle01/kotlin_client/ExampleUnitTest.kt
+++ kotlin_client/src/test/java/com/huddle01/kotlin_client/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.huddle01.kotlin_client
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
