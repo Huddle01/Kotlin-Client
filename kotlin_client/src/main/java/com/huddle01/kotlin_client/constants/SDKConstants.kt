5cc472d310c9cf5025a7920ef9255332987b1a7f
Amber Jain <jainanuamber@gmail.com>
2025-01-09 14:59:29 +0530

    updated wrt kotlin@version

--- kotlin_client/src/main/java/com/huddle01/kotlin_client/constants/SDKConstants.kt
+++ kotlin_client/src/main/java/com/huddle01/kotlin_client/constants/SDKConstants.kt
@@ -1,6 +1,6 @@
 package com.huddle01.kotlin_client.constants
 
 object SDKConstants {
-    const val SDK_VERSION: String = "1.1.5"
+    const val SDK_VERSION: String = "1.1.6"
     const val MAX_DATA_MESSAGE_SIZE: Int = 1 * 1024
 }
